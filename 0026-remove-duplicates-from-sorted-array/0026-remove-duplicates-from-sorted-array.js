c2b7b8224e9d56b63d72bff2a8198ff66d614bb9
Nguyễn Công Quốc <Nguyencongquoc0904@gmail.com>
2026-06-25 11:03:03 +0700

    Time: 59 ms (5.9%), Space: 59.1 MB (5.66%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    if (!nums.length) return 0;
+    nums.sort((a, b) => a - b);
+
+    let unique = new Set();
+    let newNums = [...nums];
+    let deleteCount = 0;
+    for (let i = 0; i < newNums.length; i++) {
+        if (unique.has(newNums[i])) {
+            nums.splice(i - deleteCount, 1);
+            deleteCount++;
+        } else {
+            unique.add(newNums[i])
+        }
+    }
+
+    return nums.length;
+};

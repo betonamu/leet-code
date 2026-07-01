5b120b84da51bc97fc84b982052110786aefc314
Nguyễn Công Quốc <Nguyencongquoc0904@gmail.com>
2026-07-01 11:43:58 +0700

    Time: 69 ms (13.79%), Space: 64.3 MB (5.22%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -9,7 +9,7 @@ var strStr = function (haystack, needle) {
     let left = 0;
     for (let right = needle.length - 1; right < haystack.length; right++) {
         let currentRange = haystack.slice(left, right + 1);
-        console.log({currentRange, needle})
+        console.log({ currentRange, needle })
         if (currentRange === needle) {
             return left;
         } else {
@@ -18,3 +18,8 @@ var strStr = function (haystack, needle) {
     }
     return -1;
 };
+
+//đầu tiên cho left = 0, và right = needle length - 1.
+//bởi vì như z thì ta đã tạo ra 1 range chưa từ ta muốn tìm
+//tăng left và right cho đến khi tìm thấy từ ta cần 
+//kh tìm thấy thì return -1

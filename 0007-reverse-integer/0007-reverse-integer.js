4f617236bae36b9a06c3fe99341524cf01653ef1
Nguyễn Công Quốc <Nguyencongquoc0904@gmail.com>
2026-07-03 10:30:17 +0700

    Time: 52 ms (55.35%), Space: 54.3 MB (98.83%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -18,8 +18,6 @@ var reverse = function (x) {
         right--;
     }
 
-    const MIN_INT = -Math.pow(2, 31);
-    const MAX_INT = Math.pow(2, 31) - 1;
     let reversed = Number(result.join(""));
     if (reversed < -Math.pow(2, 31) || reversed > Math.pow(2, 31) - 1) {
         return 0;
